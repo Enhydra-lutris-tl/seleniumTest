ea3501f179657131e19f12279abf15c4e749bd40
otter-life <2697863232@qq.com>
2023-03-27 17:38:59 +0800

    pyside6练习0.04

--- tktest.py
+++ tktest.py
@@ -5,7 +5,6 @@ from PySide6.QtCore import Qt, Signal, Slot, QThread, QRect, QSize
 from PySide6.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QLineEdit, QHBoxLayout, QVBoxLayout, \
     QStyle, QTextEdit, QMessageBox
 
-
 import sys
 from untitled import Ui_Form
 
@@ -19,8 +18,21 @@ class Main(QMainWindow):
         self.ui.setupUi(self)
 
 
+class QSSLoader:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def read_qss_file(qss_file_name):
+        with open(qss_file_name, 'r', encoding='UTF-8') as file:
+            return file.read()
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     main = Main()
+    # style_file = './index.qss'
+    # style_sheet = QSSLoader.read_qss_file(style_file)
+    # main.setStyleSheet(style_sheet)
     main.show()
     sys.exit(app.exec())

fe72033d8dfb165eccfb23201eea354e94868f9c
otter-life <2697863232@qq.com>
2023-03-31 17:48:27 +0800

    记账功能0.0.1

--- wanwan.py
+++ wanwan.py
@@ -1,19 +1,29 @@
 import pandas as pd
+from prettytable import PrettyTable
 
 
 def main():
-    path = open('F:/记账测试/alipay_record_20221104_115554.csv')
+    path = open('/Users/tanglei/life/alipay_record_20230331_103351.csv')
     df = pd.read_csv(path)
-    df2 = df.reset_index()
+    df2 = df.drop(range(23))
+    # print(df2)
     count = df2.shape[1]
     cl = df2.columns
     newcl = []
     for i in range(count):
         newcl.append(df2.iloc[0][cl[i]])
-
-    df3 = df2.set_axis(newcl,axis=1)
-    print(df3.drop(labels=0))
-
+    df3 = df2.set_axis(newcl, axis=1).reset_index().drop(labels=0).drop(labels="index",axis=1)
+    # print(df3)
+    # 表格库使用
+    cl3 = df3.columns
+    count3 = df3.shape[1]
+    table = PrettyTable(newcl)
+    for i in range(df3.shape[0]):
+        ceshi = []
+        for b in range(count3):
+            ceshi.append(df3.iloc[i][cl3[b]])
+        table.add_row(ceshi)
+    print(table)
 
 
 main()
